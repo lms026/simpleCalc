6699ba386c37d89c0327c0a7976e097261388a83
lms026 <suk27430418@gmail.com>
2025-05-20 09:45:34 +0900

    modified

--- main.py
+++ main.py
@@ -2,11 +2,11 @@
 
 # This function adds two numbers
 def add(x, y):
-    return x + y
+    return x - y
 
 # This function subtracts two numbers
 def subtract(x, y):
-    return x - y
+    return x + y
 
 
 # This function multiplies two numbers
